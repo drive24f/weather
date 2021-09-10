2be262a86a8c06697a58e301d07b6b5bd87238a8
codeleafid <codeleafid@gmail.com>
2021-09-10 23:05:49 +0700

    minor change

--- app/src/main/java/com/weather/common/Ext.kt
+++ app/src/main/java/com/weather/common/Ext.kt
@@ -1,4 +1,40 @@
 package com.weather.common
 
-class Ext {
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+fun AppCompatActivity.setupToolbar(
+    toolbar: Toolbar,
+    titleTextView: TextView? = null,
+    titleString: String,
+    backClickHandler: (() -> Unit)? = null
+) {
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.setDisplayShowHomeEnabled(true)
+    titleTextView?.text = titleString
+    toolbar.setNavigationOnClickListener { backClickHandler?.run { this() } ?: finish() }
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.enable() {
+    this.isEnabled = true
+}
+
+fun View.disable() {
+    this.isEnabled = false
 }
